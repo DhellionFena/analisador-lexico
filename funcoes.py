d906c4091e39a79c624b413ab6380ea09710f7b8
Antônio César <antonio.contato.eng@gmail.com>
2023-03-10 20:30:28 -0300

    continuacao da logica e correcao de erros

--- funcoes.py
+++ funcoes.py
@@ -6,24 +6,59 @@ def getAlgoritmo(nome_do_arquivo):
         #print(texto)
     return texto
 
-'''
-TODO:
-- Rever a logica de como identificar as palavras pois as variaveis que nao existem podem atrapalhar na hora de reconhecer um tipo.
-    Exemplo:
-        caso eu crie uma variavel "i", e depois eu vou fazer uma declaração "int b" no codigo. Da forma como está escrito,
-        o analizador vai encontrar o "i" de int e achar que é uma variavel solta "i" que eu declarei anteriormente.
 
-- Implementar no lugar dos prints a inserção da tabela de símbolos e o inicio do fluxo de uma linguagem intermediaria.
-- Implementar na verificação de criação de variavel a validação Regex que Racoba criou
-'''
-def analizado_lexico(texto):
+def separador(palavra, caractere):
+    i = palavra.find(caractere)
+    palavra_nova = palavra[:i] + ' ' + palavra[i] + ' ' +palavra[i+1:]
+    return palavra_nova
+
+
+def separar_texto(lista):
+    caracteres = [
+                ';', '"', '(', ')', '.', '>',  '<',  '~', '=', '+', '-',
+                '*', '/', '[', ']', '&',  '%',  '$', '@',  '!',  '?', ',', 
+                ':', '{', '}'
+                ]
+
+    texto_separado = []
+    for palavra in lista:
+        nova_lista = []
+        for c in caracteres:
+            if c in palavra:
+                palavra = separador(palavra, c)
+        p = palavra.split()
+        for item in p:
+            texto_separado.append(item)
+
+    return texto_separado
+
+def get_simbolo(tabela_de_simbolos, simbolo):
+    for s in tabela_de_simbolos:
+        if s[0] == simbolo:
+            return (s[0], s[1])
+    return None
+
+def analizador_lexico(texto):
+    fluxo_codigo = []
+    tabela_de_simbolos = []
+    cont = 0
     for palavra in texto:
-        aux = ''
-        for letra in palavra:
-            aux += letra
-            if letra in tipos:
-                print(f"Achei {letra}! seu tipo eh {tipos[letra]}")
-            elif aux in tipos:
-                print(f"Achei {aux}! seu tipo eh {tipos[aux]}")
+        if palavra in tipos:
+            fluxo_codigo.append((palavra, tipos[palavra]))
+        else:
+            simbolo = get_simbolo(tabela_de_simbolos, palavra)
+            if simbolo:
+                fluxo_codigo.append((str(simbolo[1]), '99'))
             else:
-                print(f'nao achei ainda o que significa {aux}')
+                #TODO VERIFICAR SE A VARIAVEL PODE SER UMA VARIAVEL E SE NAO INFORMAR UM ERRO!!!!
+                tabela_de_simbolos.append((palavra, cont))
+                fluxo_codigo.append(cont, '99')
+                cont += 1
+
+    print("Fluxo de codigo:")
+    print(fluxo_codigo)
+
+    print("Tabela de Simbolos:")
+    print(tabela_de_simbolos)
+
+    #TODO Exportar esses prints como documentos de texto.
