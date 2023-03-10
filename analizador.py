8ad561936533f11715faced310de3f5b7bba5093
Antônio César <antonio.contato.eng@gmail.com>
2023-03-10 20:30:51 -0300

    continuacao da logica

--- analizador.py
+++ analizador.py
@@ -1,10 +1,8 @@
-from funcoes import getAlgoritmo, analizado_lexico
+from funcoes import getAlgoritmo, analizador_lexico, separar_texto
 
 # AQUI É A MAIN DO NOSSO PROGRAMA
 
 texto = getAlgoritmo('racobaldo.txt')       # Pegando o texto do algoritmo
 texto = texto.split()                       # Separando a string e transformando em Lista
-analizado_lexico(texto)                     # Aqui é onde a magica acontece  >:)
-
-
-# FIM (depois a gente cria um menuzinho pra ficar top).
+texto = separar_texto(texto)                # Limpando a lista dos caracteres especiais
+analizador_lexico(texto)                    # Aqui é onde a magica acontece  >:)
--- tipos.py
+++ tipos.py
@@ -1,9 +1,14 @@
 #TODO: Adicionar TODAS AS PALAVRAS CHAVES faltantes
 tipos = {
-    "int": "01",        # atribuição de tipo
+    "numerico": "01",        # atribuição de tipo
+    "vetor": "01",        # atribuição de tipo
+    "caracter": "01",        # atribuição de tipo
     ";": "02",          # fim da linha
     "=": "03",          # atribuição de valor
     "+": "04",          # operador matematico
+    "-": "04",          # operador matematico
+    "*": "04",          # operador matematico
+    "/": "04",          # operador matematico
     "se": "05",         # inicio de condicao
     "entao": "06",      # fim de condicao e abertura de escopo
     "senao": "07",      # inverso de condicao
@@ -12,4 +17,10 @@ tipos = {
     ")": "10",          # encerramento de conteudo
     "escreva": "11",    # funcao de escrita
     '"': "12",          # aspas (go LOUD)
+    'para': "12",       # inicio de loop
+    'cada': "12",       # inicio de loop
+    'em': "12",       # inicio de loop
 }
+
+# 99 PARA ROTULOS
+# 98 PARA NUMEROS
